9a85eb8f1cf82c2cbca8273c4c2bcbae149ddf0f
danagilliann <danalee96@gmail.com>
2017-06-09 16:42:44 -0400

    soundcloud api low key sucks

--- play_the_list.py
+++ play_the_list.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask import render_template
+import config
+import soundcloud
+
+client_id = config.client_id()
+client_secret = config.client_secret()
+client = soundcloud.Client(client_id=client_id)
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
+    # track = client.get('/resolve', url='http://soundcloud.com/forss/flickermood')
+    # print(track.id)
+    tracks = client.get('/tracks', limit=10)
+    for track in tracks:
+        print(track.title)
     return render_template('main.html')
 
 # @app.route('/omg', strict_slashes=False)
