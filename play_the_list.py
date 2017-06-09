337097f344836e2ca72ea79adbb0cd3e5660768b
danagilliann <danalee96@gmail.com>
2017-06-09 17:03:03 -0400

    got playlists working

--- play_the_list.py
+++ play_the_list.py
@@ -1,21 +1,29 @@
+# libraries
 from flask import Flask
 from flask import render_template
-import config
 import soundcloud
 
-client_id = config.client_id()
-client_secret = config.client_secret()
-client = soundcloud.Client(client_id=client_id)
+# dev
+from pprint import pprint
+import config
 
+client = soundcloud.Client(
+        client_id=config.client_id(),
+        client_secret=config.client_secret(),
+        username=config.username(),
+        password=config.password()
+)
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     # track = client.get('/resolve', url='http://soundcloud.com/forss/flickermood')
     # print(track.id)
-    tracks = client.get('/tracks', limit=10)
-    for track in tracks:
-        print(track.title)
+    playlist = client.get('/resolve', url='')
+    pprint(vars(playlist))
+    # tracks = client.get('/tracks', limit=10)
+    # for track in tracks:
+    #     print(track.title)
     return render_template('main.html')
 
 # @app.route('/omg', strict_slashes=False)
